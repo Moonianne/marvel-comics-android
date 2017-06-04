de6820cefac73d70ad6705dc0bd213110995bbc2
Segun Famisa <segunfamisa@gmail.com>
2017-06-04 15:38:25 +0100

    [chore] Create SchedulerProvider
    - This scheduler provider will be used to provide specific prefered scheduler,
    will be helpful with testing

---
+++ app/src/main/java/com/segunfamisa/sample/comics/common/scheduler/SchedulerProvider.java
@@ -0,0 +1,19 @@
+package com.segunfamisa.sample.comics.common.scheduler;
+
+
+import java.util.concurrent.Executor;
+
+import io.reactivex.Scheduler;
+
+/**
+ * Interface defining the RxJava Scheduler provider.
+ */
+interface SchedulerProvider {
+
+    Scheduler io();
+    Scheduler mainThread();
+    Scheduler computation();
+    Scheduler trampoline();
+    Scheduler from(Executor executor);
+
+}
