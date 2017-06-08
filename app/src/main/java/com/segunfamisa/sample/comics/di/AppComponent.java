c9210e2f3c258f8ee0b7fbd3b0ca09bc68d8921a
Segun Famisa <segunfamisa@gmail.com>
2017-06-09 00:58:24 +0100

    [chore] Add dependency injection for Budget screen

--- app/src/main/java/com/segunfamisa/sample/comics/di/AppComponent.java
+++ app/src/main/java/com/segunfamisa/sample/comics/di/AppComponent.java
@@ -2,6 +2,8 @@ package com.segunfamisa.sample.comics.di;
 
 import com.segunfamisa.sample.comics.data.di.ApiModule;
 import com.segunfamisa.sample.comics.data.di.ComicRepositoryModule;
+import com.segunfamisa.sample.comics.presentation.budget.di.BudgetComponent;
+import com.segunfamisa.sample.comics.presentation.budget.di.BudgetPresenterModule;
 import com.segunfamisa.sample.comics.presentation.comicdetails.di.ComicDetailsComponent;
 import com.segunfamisa.sample.comics.presentation.comicdetails.di.ComicDetailsPresenterModule;
 import com.segunfamisa.sample.comics.presentation.comiclist.di.ComicListComponent;
@@ -15,6 +17,8 @@ import dagger.Component;
 @Singleton
 @Component(modules = { ComicRepositoryModule.class, ApiModule.class, AppModule.class})
 public interface AppComponent {
+
+    BudgetComponent plus(BudgetPresenterModule module);
 
     ComicDetailsComponent plus(ComicDetailsPresenterModule module);
 
